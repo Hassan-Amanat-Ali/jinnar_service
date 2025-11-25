c4ce8c6da1c25fb5383259bd2f06c8c77744dd27
Mughees ur Rehman <153646093+Mogehs@users.noreply.github.com>
2025-11-25 14:04:17 +0500

    Refactor Help component to include HelpAndSupport

--- src/pages/common/Help.jsx
+++ src/pages/common/Help.jsx
@@ -1,5 +1,11 @@
+import HelpAndSupport from "../../components/profile/HelpAndSupport";
+
 const Help = () => {
-  return <div>Help</div>;
+  return (
+    <div className="mt-20 px-10 mx-auto">
+      <HelpAndSupport />
+    </div>
+  );
 };
 
 export default Help;
